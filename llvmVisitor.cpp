403eee46c28d9ed93f7222d2c1867c9560dbfd22
Ethano1998 <ethan.oualid@campus.technion.ac.il>
2025-02-02 09:36:39 +0200

    changement du traitement de exp

--- llvmVisitor.cpp
+++ llvmVisitor.cpp
@@ -42,8 +42,8 @@ void LlvmVisitor::visit(ast::Cast &node){
 
 void LlvmVisitor::visit(ast::Formals &node){
     for(const auto &formal : node.formals){
-                formal->accept(*this);
-            }
+        formal->accept(*this);
+    }
 }
 
 void LlvmVisitor::visit(ast::Formal &node){
@@ -68,14 +68,17 @@ void LlvmVisitor::visit(ast::VarDecl &node){
     std::string reg_ptr = code_buffer.freshVar();
     code_buffer.emit(reg_ptr + " = getelementptr [50 x i32], [50 x i32]* %Array, i32 0, i32 " + std::to_string(node.id->offset) + "\n");
     if(node.init_exp != nullptr){
-        code_buffer.emit("store i32 ");
+        std::string exp_var = code_buffer.freshVar();
+        code_buffer.emit(exp_var + " = ");
         node.init_exp->accept(*this);
-        code_buffer.emit(", i32* "+ reg_ptr + "\n");
+        code_buffer.emit("store i32 exp_var, i32* "+ reg_ptr + "\n");
     }
 
 }
 
 void LlvmVisitor::visit(ast::Assign &node){
+    std::string reg_ptr = code_buffer.freshVar();
+    code_buffer.emit(reg_ptr + " = getelementptr [50 x i32], [50 x i32]* %Array, i32 0, i32 " + std::to_string(node.id->offset) + "\n");
 }
 
 void LlvmVisitor::visit(ast::Call &node){
@@ -110,7 +113,6 @@ void LlvmVisitor::visit(ast::FuncDecl &node){
     node.formals->accept(*this);
     node.body->accept(*this);
     code_buffer.emit("}\n");
-   
 }
 
 void LlvmVisitor::visit(ast::Funcs &node){
